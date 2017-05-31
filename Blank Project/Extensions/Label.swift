fcd46a1d4e424ef9d8d9d7efee02049a8d49f1b1
Chi Phuong <vo.nguyen.chi.phuong@framgia.com>
2017-05-31 09:37:52 +0700

    Add japanes label

---
+++ Blank Project/Extensions/Label.swift
@@ -0,0 +1,22 @@
+//
+//  Label.swift
+//  Blank Project
+//
+//  Created by framgia on 5/31/17.
+//  Copyright © 2017 Vo Nguyen Chi Phuong. All rights reserved.
+//
+
+import UIKit
+
+class Label: UILabel {
+    var edgeInsets: UIEdgeInsets = UIEdgeInsets.init(top: -1.24, left: 0, bottom: 0, right: 0)
+    override func textRect(forBounds bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
+        var rect = super.textRect(forBounds: UIEdgeInsetsInsetRect(bounds, edgeInsets), limitedToNumberOfLines: numberOfLines)
+        rect.size.height += 2
+        return rect
+    }
+
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: UIEdgeInsetsInsetRect(rect, edgeInsets))
+    }
+}
